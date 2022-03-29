df9eb96c6374759bf8fd4ae44aa315f6bca2b95b
ChrisMisztur <chris@mriiot.com>
2022-03-29 11:13:41 -0500

    * standup script

--- scripts/standup.sh
+++ scripts/standup.sh
@@ -3,11 +3,26 @@
 mkdir ~/fanuc
 cd ~/fanuc
 
+# stop and remove containers
+docker container stop fanuc_driver
+docker container stop agent
+docker container rm fanuc_driver
+docker container rm agent
+docker container prune
+
+# remove images
+docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | grep 'ladder99/fanuc-driver')
+docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | grep 'ladder99/agent')
+docker image prune
+
+# remove directories
 sudo rm -rf volumes
 sudo rm -rf fanuc-driver
 
+# clone repo
 git clone --recurse-submodules -j8 https://github.com/Ladder99/fanuc-driver.git
 
+# checkout develop branch
 cd fanuc-driver
 git checkout develop
 cd base-driver
@@ -15,18 +30,21 @@ git checkout develop
 
 cd ~/fanuc/fanuc-driver
 
+# set vars for build
 profile=mtc
 os=LINUX64
 image=ladder99/fanuc-driver
 commit=$(git rev-parse --short HEAD)
 tag=linux64-develop-$commit
 
+# build driver container
 docker build \
 	-f Dockerfile.$os \
   --tag=$image:$tag .
 
 cd ~/fanuc/fanuc-driver/examples
 
+# prepare volumes
 mkdir -p ../../volumes/fanuc-driver
 cp fanuc-driver/nlog-example-linux.config ../../volumes/fanuc-driver/nlog.config
 cp fanuc-driver/config-example.yml ../../volumes/fanuc-driver/config.yml
@@ -41,8 +59,9 @@ cp mtconnect/agent.cfg ../../volumes/agent/agent.cfg
 cp mtconnect/devices_example.xml ../../volumes/agent/devices.xml
 
 # the architecture you are on (linux64,arm)
-# FOCAS_TGT sets the docker image tag
+# set the driver image tag
 #export FOCAS_TGT=$( docker images ladder99/fanuc-driver | tail -n +2 | awk 'NR==1{print $1":"$2}' )
 export FOCAS_TGT=$tag
 
+# standup profile
 docker-compose -f compose.yml --profile $profile up -d
